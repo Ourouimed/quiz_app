987fb3f3fe7ede8f9322917622b27d03ce09e027
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2024-12-29 10:46:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ const CreateQuestions = (data)=>{
                 <svg viewBox="0 0 36 36" class="circular-chart orange">
                     <path class="circle-bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/>
                     <path class="circle" stroke-dasharray="${(total*100)/data.length}, 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                    <text x="18" y="20.35" class="percentage">${(total*100)/data.length}%</text>
+                    <text x="18" y="20.35" class="percentage">${Math.floor((total*100)/data.length)}%</text>
                 </svg>
             </div>
             <p class="my-2 text-center">Your have <span class="font-bold text-cyan-500">${total}/${data.length}</span> correct answers
